4df84785615f4b6da507e3819573b51998284b4b
zhamiliaNamyrova <74160914+zhamiliaNamyrova@users.noreply.github.com>
2020-11-12 10:44:51 -0500

    working on nice message

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Git last day");
-
-
-
-
-
-
-
+        //b21 was excited
     }
 }
