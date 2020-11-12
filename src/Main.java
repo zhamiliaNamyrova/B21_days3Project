955c6421d900b63b6e0e8b116d214329d33a9f8e
zhamiliaNamyrova <74160914+zhamiliaNamyrova@users.noreply.github.com>
2020-11-12 10:48:20 -0500

    done on project

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("Hello Git last day");
         //b21 was excited
         //b21 is nice batch dedicated
+        // b21 is successful tester
+        // b21 best tester
     }
 }
